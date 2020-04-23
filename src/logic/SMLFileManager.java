1037eda7e4534c79ad564681718306ef7958b23c
Fernando Estevez <fernandoestevez8494@gmail.com>
2020-04-23 00:38:45 -0400

    Created to be the main class

---
+++ src/logic/SMLFileManager.java
@@ -0,0 +1,15 @@
+package logic;
+
+import gui.*;
+
+public class SMLFileManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MainWindow main = new MainWindow();
+		main.setVisible(true);
+
+	}
+
+}
